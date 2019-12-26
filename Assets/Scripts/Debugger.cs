d9db5bfec7c236674d448560b1a554b18a276e1b
turnera30 <turnermallen30@gmail.com>
2019-12-26 12:32:27 -0600

    Added Debugging Class

---
+++ Assets/Scripts/Debugger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour
+{
+    static public void PrintRed(string message)
+    {
+        Debug.Log("<color=red>" + message + "</color>");
+    }
+}

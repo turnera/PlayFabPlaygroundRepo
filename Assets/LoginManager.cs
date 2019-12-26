ff7d3680d6d254c0ab9f46bf0db722f03081ac75
turnera30 <turnermallen30@gmail.com>
2019-12-26 11:07:45 -0600

    Initial commit

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    TestScript testScript = null;
+
+    private void Awake() 
+    {
+        testScript = GetComponent<TestScript>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    private void Login()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "D8E6D";
+        }
+        
+        PlayFabClientAPI.LoginWithCustomID(new LoginWithCustomIDRequest
+        {
+            CreateAccount = true,
+            TitleId = PlayFabSettings.TitleId,
+            CustomId = "TestCustomID"
+        },
+        OnLoginSuccess, OnSharedError);
+    }
+
+    private void OnSharedError(PlayFabError obj)
+    {
+        Debug.Log(obj.GenerateErrorReport());
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        testScript.RunTest();
+    }
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    
+    public void RunTest()
+    {
+        GetStats();
+    }
+
+    public void GetStats() 
+    {
+        PlayFab.PlayFabClientAPI.GetPlayerStatistics(new GetPlayerStatisticsRequest(), 
+        OnGetStats, OnSharedError);
+    }
+
+    private void OnSharedError(PlayFabError obj)
+    {
+        Debug.Log(obj.GenerateErrorReport());
+    }
+
+    private void OnGetStats(GetPlayerStatisticsResult result)
+    {
+        Debug.Log("Received the following Statistics:");
+        foreach(var eachStat in result.Statistics)
+        {
+            Debug.Log("Statistic (" + eachStat.StatisticName + "):" + eachStat.Value);
+        }
+    }
+}
